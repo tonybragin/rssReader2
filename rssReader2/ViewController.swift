581a9703541f1c0f49b910050507220bb3ab5607
Anton Bragin <antonbragin28@yandex.ru>
2020-04-10 21:35:40 +0700

    Initial Commit

---
+++ rssReader2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rssReader2
+//
+//  Created by Tony on 10/04/2020.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
